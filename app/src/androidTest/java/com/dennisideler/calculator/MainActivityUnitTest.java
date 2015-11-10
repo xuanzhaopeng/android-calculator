351f169babcd6d19f9e244284847bc242812e201
xuanzhaopeng <xuanzhaopeng@gmail.com>
2015-11-10 16:45:40 +0100

    Complete test for button Label check

--- app/src/androidTest/java/com/dennisideler/calculator/MainActivityUnitTest.java
+++ app/src/androidTest/java/com/dennisideler/calculator/MainActivityUnitTest.java
@@ -1,12 +1,15 @@
 package com.dennisideler.calculator;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.test.ActivityUnitTestCase;
 import android.test.UiThreadTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.widget.Button;
 
 import com.dennisideler.calculator.wrapper.ButtonWrapper;
+
+import javax.annotation.Resource;
 
 
 /**
@@ -37,7 +40,7 @@ public class MainActivityUnitTest extends ActivityUnitTestCase<MainActivity> {
 
     @MediumTest
     @UiThreadTest
-    public void testButton()
+    public void testButtonLabel()
     {
         MainActivity activity = startActivity(mLaunchIntent, null,null);
 
@@ -47,7 +50,55 @@ public class MainActivityUnitTest extends ActivityUnitTestCase<MainActivity> {
 
         ButtonWrapper.Create((Button)activity.findViewById(R.id.button1))
                 .shouldTextEqualsTo("1")
-        .shouldBeClickAble();
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button2))
+                .shouldTextEqualsTo("2")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button3))
+                .shouldTextEqualsTo("3")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button4))
+                .shouldTextEqualsTo("4")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button5))
+                .shouldTextEqualsTo("5")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button6))
+                .shouldTextEqualsTo("6")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button7))
+                .shouldTextEqualsTo("7")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button8))
+                .shouldTextEqualsTo("8")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.button9))
+                .shouldTextEqualsTo("9")
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.buttonAdd))
+                .shouldTextEqualsTo(MainActivity.ADD)
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.buttonDiv))
+                .shouldTextEqualsTo(MainActivity.DIV)
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.buttonSub))
+                .shouldTextEqualsTo(MainActivity.SUB)
+                .shouldBeClickAble();
+
+        ButtonWrapper.Create((Button)activity.findViewById(R.id.buttonMul))
+                .shouldTextEqualsTo(MainActivity.MUL)
+                .shouldBeClickAble();
 
     }
 }
