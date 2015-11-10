1400b336dad86ad8ba7777a319c421f06aa495a4
xuanzhaopeng <xuanzhaopeng@gmail.com>
2015-11-10 13:53:14 +0100

    add missing tag of ActivityUnitTest

--- app/src/androidTest/java/com/dennisideler/calculator/MainActivityUnitTest.java
+++ app/src/androidTest/java/com/dennisideler/calculator/MainActivityUnitTest.java
@@ -1,22 +1,14 @@
 package com.dennisideler.calculator;
 
-import org.junit.Test;
-import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
-
-import android.support.test.runner.AndroidJUnitRunner;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
-import android.test.suitebuilder.annotation.LargeTest;
+import android.test.UiThreadTest;
 import android.test.suitebuilder.annotation.MediumTest;
-
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
 
 
 /**
  * Created by zxuan on 2015/11/9.
  */
-@LargeTest
 public class MainActivityUnitTest extends ActivityUnitTestCase<MainActivity> {
 
     private Intent mLaunchIntent;
@@ -29,14 +21,15 @@ public class MainActivityUnitTest extends ActivityUnitTestCase<MainActivity> {
     public void setUp() throws Exception
     {
         super.setUp();
-        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        mLaunchIntent = new Intent(Intent.ACTION_MAIN);
     }
 
     @MediumTest
+    @UiThreadTest
     public void testPreconditions()
     {
         startActivity(mLaunchIntent, null, null);
-        assertThat(getActivity(), notNullValue());
+        assertNotNull("The main activity is null", getActivity());
     }
 
 
